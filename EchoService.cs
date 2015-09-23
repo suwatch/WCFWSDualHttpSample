04220adc54062ee2971058a889c0096a58396081
Suwath Ch <suwatch@microsoft.com>
2015-09-23 09:41:00 -0700

    initial commit

---
+++ EchoService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.ServiceModel.Activation;
+
+namespace WCFBasicHttpSample
+{
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class EchoService : IEcho
+    {
+        public string Echo(string message)
+        {
+            return message;
+        }
+
+        public Stream DownloadStream()
+        {
+            byte[] buffer = new byte[1024];
+            Random rand = new Random();
+            rand.NextBytes(buffer);
+
+            MemoryStream stream = new MemoryStream(buffer);
+            return stream;
+        }
+
+        public void UploadStream(Stream stream)
+        {
+            int readResult;
+            int bytesRead = 0;
+            byte[] buffer = new byte[1000];
+            do
+            {
+                readResult = stream.Read(buffer, 0, buffer.Length);
+                bytesRead += readResult;
+            }
+            while (readResult != 0);
+
+            stream.Close();
+        }
+    }
+}
---
+++ IEcho.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.ServiceModel;
+
+namespace WCFBasicHttpSample
+{
+    [ServiceContract]
+    public interface IEcho
+    {
+        [OperationContract]
+        string Echo(string message);
+
+        [OperationContract]
+        Stream DownloadStream();
+
+        [OperationContract]
+        void UploadStream(Stream stream);
+    }
+}
